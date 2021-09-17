178282a021fb022d53bd6df1126b37ae1daebb54
Federico Decono <fdecono@gmail.com>
2021-09-17 10:26:08 -0300

    Render content, error message or loader

---
+++ src/Loader.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Loader = () => {
+  return (
+    <div className="ui segment">
+      <div className="ui active inverted dimmer">
+        <div className="ui text loader">
+          Loading
+        </div>
+      </div>
+      <p></p>
+    </div>
+  );
+}
+
+export default Loader
--- src/index.js
+++ src/index.js
@@ -1,22 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Loader from './Loader';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      lat: null
+      lat: null,
+      errorMessage: ''
     };
     window.navigator.geolocation.getCurrentPosition(
       (position) => {
         this.setState({lat: position.coords.latitude});
       },
-      (error) => console.log(error)
+      (error) => {
+        this.setState({errorMessage: error.message});
+      }
     );
   }
 
   render() {
-    return <div>{this.state.lat}</div>;
+    // return (
+    //   <div>
+    //     Latitude: {this.state.lat}
+    //     <br />
+    //     Error: {this.state.errorMessage}
+    //   </div>
+    // );
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>
+    } else if (!this.state.errorMessage && this.state.lat) {
+      return <div>Latitude: {this.state.lat}</div>
+    } else {
+      return <Loader />
+    }
   }
 }
 
